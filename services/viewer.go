f89f4876f63babc345836e33c7f479c5e66fd7ce
Louis Heal <louisheal@live.co.uk>
2024-12-18 17:07:26 +0000

    return default item when no item is selected

--- services/viewer.go
+++ services/viewer.go
@@ -25,16 +25,10 @@ func NewViewerService(
 }
 
 func (s *ViewerService) GetViewer(userID, channelID models.TwitchID, username string) (Viewer, error) {
-	// Check if userID exists in users table
-	// Create user if not exists
-
 	item, err := s.Items.GetSelectedItem(userID, channelID)
 	if err != nil {
 		return Viewer{}, err
 	}
-
-	// Check if user has selected an item
-	// Retrieve channel's default item if not
 
 	return Viewer{UserID: userID, Username: username, Image: item.Image}, nil
 }
