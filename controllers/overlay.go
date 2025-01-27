d54e2dee9583e15e18690afe9554799c1687da0b
louisheal <louisheal@live.co.uk>
2025-01-27 14:46:49 +0000

    add transfer encoding chunked header

--- controllers/overlay.go
+++ controllers/overlay.go
@@ -76,6 +76,7 @@ func (c *OverlayController) HandleListen(ctx *gin.Context) {
 	ctx.Header("Content-Type", "text/event-stream")
 	ctx.Header("Cache-Control", "no-cache")
 	ctx.Header("Connection", "keep-alive")
+	ctx.Header("Transfer-Encoding", "chunked")
 
 	ctx.Stream(func(w io.Writer) bool {
 		select {
