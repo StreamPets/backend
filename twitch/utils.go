a9ed9cc6b2dfd24c9e0754b0b3c74b4653714f44
louisheal <louisheal@live.co.uk>
2025-02-05 13:15:56 +0000

    document parse function

--- twitch/utils.go
+++ twitch/utils.go
@@ -10,6 +10,12 @@ import (
 // Indicates an invalid Twitch user access token.
 var ErrInvalidUserToken error = errors.New("invalid access token")
 
+// Unmarshals a response body into a specified struct.
+//
+//	var data dataStruct
+//	if err = parseResponse(&data, response); err != nil {
+//		return err
+//	}
 func parseResponse(data interface{}, resp *http.Response) error {
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
